535ebc22b439a593a15b54d6228cd19092874cf2
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2025-10-14 19:59:54 +1000

    Tidied explain and added more comments

--- summary_explain.py
+++ summary_explain.py
@@ -13,45 +13,44 @@ weather_data = [
     ["2020-06-26T07:00:00+08:00", 53, 66],
     ]
 
-min_list = []
-max_list = []
-date = len(weather_data)
+min_list = [] #empty list for low temps
+max_list = [] #empty list for high temps
+date = len(weather_data) #number of days in dataset
 
 #ADD LOW AND HIGH TEMP DATA TO NEW, SEPERATE LISTS
 for data in weather_data:
     if data and len(data) == 3:
-        min_list.append(data[1]) #add low temps to new list
-        max_list.append(data[2]) #add high temps to new list
+        min_list.append(data[1]) #add low temps to min_list
+        max_list.append(data[2]) #add high temps to max_list
 
-#FIND MEAN HIGH AND LOW TEMPS
-mean_low = weather.calculate_mean(min_list) #find mean temp
-mean_low_celcius = weather.convert_f_to_c(mean_low) #convert to celcius
-avg_low = weather.format_temperature(mean_low_celcius) #format degrees symbol
-mean_high = weather.calculate_mean(max_list) #find mean temp
-mean_high_celcius = weather.convert_f_to_c(mean_high) #convert to celcius
-avg_high = weather.format_temperature(mean_high_celcius) #format degrees symbol
+#FIND MEAN (AVG) LOW TEMP
+mean_low_f = weather.calculate_mean(min_list) #find mean temp (farenheit)
+mean_low_c = weather.convert_f_to_c(mean_low_f) #convert farenheit to celcius
+avg_low = weather.format_temperature(mean_low_c) #format degrees symbol (e.g. 23°C)
 
-#FIND MIN TEMP AND ADD TO LIST, THEN CONVERT AND FORMAT
-min = weather.find_min(min_list)
-min_list.extend(min) #Add min temp and index to min_list
-min_index = min_list[-1] #index for date is now last number of min_list
-min_farenheit = min_list[-2] #get temp inside min_list
-min_celcius = weather.convert_f_to_c(min_farenheit) #convert to celcius
-min_temp = weather.format_temperature(min_celcius) #format degrees symbol
+#FIND MEAN (AVG) HIGH TEMP
+mean_high_f = weather.calculate_mean(max_list) #find mean temp (fanrenheit)
+mean_high_c = weather.convert_f_to_c(mean_high_f) #convert farenheit to celcius
+avg_high = weather.format_temperature(mean_high_c) #format degrees symbol (e.g. 23°C)
 
-#FIND MAX TEMP AND ADD TO LIST, THEN CONVERT AND FORMAT
-max = weather.find_max(max_list)
-max_list.extend(max) #add max temp and index to max_list
-max_index = max_list[-1] #index for date is now last number of max_list
-max_farenheit = max_list[-2] #get temp inside max_list
-max_celcius = weather.convert_f_to_c(max_farenheit) #convert to celcius
-max_temp = weather.format_temperature(max_celcius) #format degrees symbol
+#FIND MIN TEMP, CONVERT & FORMAT
+min_temp_f, min_index = weather.find_min(min_list) #this function returns a tuple (temp, index), so we are defining both here.
+min_temp_c = weather.convert_f_to_c(min_temp_f) #convert to celcius
+min_temp = weather.format_temperature(min_temp_c) #format degrees symbol
 
-#PULL DATE FROM ORIGINAL LIST FOR HIGHEST AND LOWEST TEMPS
-min_data = weather_data[min_index][0] #min_index grabs index no. to access date of min temp inside original weather_data
-min_date = weather.convert_date(min_data) #convert date from ISO date
-max_data = weather_data[max_index][0] #max_index grabs index no. to access date of max temp inside original weather_data
-max_date = weather.convert_date(max_data) #convert date from ISO date
+#FIND MAX TEMP, CONVERT & FORMAT
+max_temp_f, max_index = weather.find_max(max_list) #this function returns a tuple (temp, index), so we are defining both here.
+max_temp_c = weather.convert_f_to_c(max_temp_f) #convert to celcius
+max_temp = weather.format_temperature(max_temp_c) #format degrees symbol
+
+#DAY OF LOWEST TEMP
+#Finding the DAY that the lowest temp occurs on, using the index value from find_min function
+min_isodate = weather_data[min_index][0] #targeting weather_data list, [min_index] grabs the correct DAY sublist, [0] targets the FIRST ROW which is isodate string
+min_date = weather.convert_date(min_isodate) #convert date from ISO date
+
+#DAY OF HIGHEST TEMP
+max_isodate = weather_data[max_index][0] #targeting weather_data list, [max_index] grabs the correct DAY sublist, [0] targets the FIRST ROW which is isodate string
+max_date = weather.convert_date(max_isodate) #convert date from ISO date
 
 print(f"{date} Day Overview")
 print(f"The lowest temperature will be {min_temp}, and will occur on {min_date}.")
